5981b352f28204b19c8905ada9ec7468bdb6e510
breetva <breetva@gmail.com>
2022-07-06 14:21:27 +0200

    Minor edits

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,51 +9,46 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet var usernameTF: UITextField!
-    @IBOutlet var passwordTF: UITextField!
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        usernameTF.autocorrectionType = .no
-        passwordTF.isSecureTextEntry = true
-    }
+    private let user = "User"
+    private let password = "Password"
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
-        welcomeVC.loggedUsername = usernameTF.text!
+        welcomeVC.loggedUser = usernameTextField.text! // FIX
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    @IBAction func loginPressed() {
+        guard usernameTextField.text == user, passwordTextField.text == password else {
+            showAlert(title: "Invalid login or password",
+                      message: "Please, enter correct login or password")
+            return
+        }
+        performSegue(withIdentifier: "showWelcomeVC", sender: nil)
     }
     
     @IBAction func unwind(for unwindSegue: UIStoryboardSegue) {
-        usernameTF.text = ""
-        passwordTF.text = ""
+        usernameTextField.text = ""
+        passwordTextField.text = ""
     }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        super .touchesBegan(touches, with: event)
-//    }
-    
-    @IBAction func loginButtonPressed() {
-        if usernameTF.text == "user" && passwordTF.text == "password" {
-            return
-        } else {
-            showAlert(with: "Invalid login or password",
-                      and: "Please, enter correct login or password")
-        }
+    @IBAction func forgotCredentials(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(title: "Oops!", message: "Your name is \(user) 😉")
+            : showAlert(title: "Oops!", message: "Your password is \(password) 🤫")
     }
     
-    @IBAction func promptUsernamePressed() {
-        showAlert(with: "Oops!", and: "Your name is 'user' 😉")
-    }
-    
-    @IBAction func promptPasswordPressed() {
-        showAlert(with: "Oops!",
-                  and: "Your password is 'password' 😉")
-    }
-    
-    private func showAlert(with title: String, and message: String) {
+    private func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let confirmButton = UIAlertAction(title: "OK", style: .default) { _ in
-            self.passwordTF.text = ""
+            self.passwordTextField.text = ""
         }
         alert.addAction(confirmButton)
         present(alert, animated: true)
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -9,13 +9,42 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
-    @IBOutlet var loggedUsernameLabel: UILabel!
+    @IBOutlet var welcomeLabel: UILabel!
     
-    var loggedUsername: String!
+    var loggedUser = ""
+    
+    private let primaryColor = UIColor(
+        red: 43/255,
+        green: 155/255,
+        blue: 255/255,
+        alpha: 1
+    )
+    
+    private let secondaryColor = UIColor(
+        red: 43/255,
+        green: 255/255,
+        blue: 136/255,
+        alpha: 1
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loggedUsernameLabel.text = "Hi, \(loggedUsername!)! 👋🏻"
+        view.addVertivalGradientLayer(topColor: primaryColor,
+                                      bottomColor: secondaryColor)
+        welcomeLabel.text = "Welcome, \(loggedUser)!"
     }
 
+}
+
+// MARK: - Set background gradient
+extension UIView {
+    func addVertivalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
 }
